8d9192736b07a6794c0971043e952c846b9ac1c9
cxx <xx.ch@outlook.com>
2018-05-31 18:17:59 +0800

    Initial commit

---
+++ app/src/main/java/cc/colorcat/rvadapter/MainActivity.java
@@ -0,0 +1,18 @@
+package cc.colorcat.rvadapter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ListView listView;
+//        listView.setSelection();
+//        listView.getSelectedItemPosition()
+    }
+}
